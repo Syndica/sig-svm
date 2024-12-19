761e009ddb38afe2707775fdddcf58de66c51ce3
David Rubin <daviru007@icloud.com>
2024-12-19 01:07:51 -0800

    add some more benchmarks

--- bench/vm_bench.zig
+++ bench/vm_bench.zig
@@ -11,7 +11,7 @@ const Region = memory.Region;
 const ITERS = 1_000;
 
 pub fn main() !void {
-    const avg_ns, const num_instructions = try benchLong();
+    const avg_ns, const num_instructions = try benchAlu();
     std.debug.print("num inst: {}\n", .{num_instructions});
     std.debug.print(
         "avg: {}, avg: {}ns/inst\n",
@@ -52,11 +52,11 @@ fn benchLong() !struct { u64, u64 } {
 fn benchSimple() !struct { u64, u64 } {
     const allocator = std.heap.c_allocator;
     const input_file = try std.fs.cwd().openFile("tests/elfs/rodata_section_sbpfv1.so", .{});
-    const bytes = try input_file.readToEndAlloc(allocator, 10 * 1024);
+    const bytes = try input_file.readToEndAlloc(allocator, svm.ebpf.MAX_FILE_SIZE);
     defer allocator.free(bytes);
 
-    const elf = try Elf.parse(bytes);
-    var executable = try Executable.fromElf(&elf);
+    const elf = try Elf.parse(bytes, allocator);
+    var executable = try Executable.fromElf(allocator, &elf);
 
     const stack_memory = try allocator.alloc(u8, 4096);
     defer allocator.free(stack_memory);
@@ -71,22 +71,61 @@ fn benchSimple() !struct { u64, u64 } {
     return benchmarkVm(&executable, m, allocator);
 }
 
+fn benchAlu() !struct { u64, u64 } {
+    const allocator = std.heap.c_allocator;
+    const input_file = try std.fs.cwd().openFile("bench/alu_bench.so", .{});
+    const bytes = try input_file.readToEndAlloc(allocator, svm.ebpf.MAX_FILE_SIZE);
+    defer allocator.free(bytes);
+
+    var loader: Executable.BuiltinProgram = .{};
+    inline for (.{
+        .{ "sol_log_64_", svm.syscalls.log64 },
+    }) |entry| {
+        const name, const func = entry;
+        _ = try loader.functions.registerFunctionHashed(
+            allocator,
+            name,
+            func,
+        );
+    }
+
+    const elf = try Elf.parse(bytes, allocator, &loader);
+    var executable = try Executable.fromElf(allocator, &elf);
+
+    const stack_memory = try allocator.alloc(u8, 4096 * 64);
+    defer allocator.free(stack_memory);
+
+    const input_memory = try allocator.alloc(u64, 2);
+    defer allocator.free(input_memory);
+    @memcpy(input_memory, &[_]u64{ 750, 0 });
+
+    const m = try MemoryMap.init(&.{
+        executable.getRoRegion(),
+        Region.init(.mutable, stack_memory, memory.STACK_START),
+        Region.init(.constant, &.{}, memory.HEAP_START),
+        Region.init(.mutable, std.mem.sliceAsBytes(input_memory), memory.INPUT_START),
+    }, .v1);
+
+    return benchmarkVm(&executable, m, allocator, &loader);
+}
+
 fn benchmarkVm(
     executable: *const Executable,
     m: MemoryMap,
     allocator: std.mem.Allocator,
+    loader: *const Executable.BuiltinProgram,
 ) !struct { u64, u64 } {
     var total_ns: u64 = 0;
     var num_instructions: ?u64 = null;
 
     for (0..ITERS) |_| {
-        var loader: Executable.BuiltinProgram = .{};
-        var vm = try Vm.init(allocator, executable, m, &loader);
+        var vm = try Vm.init(allocator, executable, m, loader);
         defer vm.deinit();
 
         const start = try std.time.Instant.now();
-        std.mem.doNotOptimizeAway(vm.run());
+        std.mem.doNotOptimizeAway(try vm.run());
         const end = try std.time.Instant.now();
+
         const elapsed = end.since(start);
         total_ns += elapsed;
         if (num_instructions == null) num_instructions = vm.instruction_count;
--- gen_tests.sh
+++ gen_tests.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-CC="${HOME}/local/llvm18-sbpf/bin/clang --target=sbf-solana-solana -fno-builtin -std=c17 -O2 -Werror"
-LD="${HOME}/local/llvm18-sbpf/bin/ld.lld -z notext -shared --Bdynamic --script tests/elfs/elf.ld -e entrypoint"
+CC="${HOME}/local/llvm18-sbpf/bin/clang"
+LD="${HOME}/local/llvm18-sbpf/bin/ld.lld"
+
+C_FLAGS="
+    -target sbf-solana-solana \
+    -mcpu=v1 \
+    -fno-builtin \
+    -fPIC -fno-unwind-tables \
+    -fomit-frame-pointer -fno-exceptions\
+    -fno-asynchronous-unwind-tables \
+    -std=c23 \
+    -O2 \
+    -Werror \
+    -I ../agave/sdk/sbf/c/inc"
+
+LD_FLAGS="-z notext -shared --Bdynamic --script tests/elfs/elf.ld -e entrypoint"
+
+CC="${CC} ${C_FLAGS}"
+LD="${LD} ${LD_FLAGS}"
 
 $CC tests/standalone/sanity.c -c -o tests/standalone/sanity.o
-$LD tests/standalone/sanity.o     -o tests/standalone/sanity.so
+$LD tests/standalone/sanity.o    -o tests/standalone/sanity.so
+
+$CC bench/alu_bench.c -c -o bench/alu_bench.o
+$LD bench/alu_bench.o    -o bench/alu_bench.so
+
 
 rm tests/standalone/*.o
+rm bench/*.o
